85a0d07d5425bc8dbcc5c433ea9a02dd1c96a14e
Shubhi Tripathi <shubhi.tripathi2020@vitbhopal.ac.in>
2023-08-08 07:29:45 +0530

    ContainerWithMostWater.cpp

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,22 @@
+/*LINK: https://leetcode.com/problems/container-with-most-water/*/
+#include<iostream>
+using namespace std;
+class Solution {
+ public:
+  int maxArea(vector<int>& height) {
+    int ans = 0;
+    int l = 0;
+    int r = height.size() - 1;
+
+    while (l < r) {
+      const int minHeight = min(height[l], height[r]);
+      ans = max(ans, minHeight * (r - l));
+      if (height[l] < height[r])
+        ++l;
+      else
+        --r;
+    }
+
+    return ans;
+  }
+};
