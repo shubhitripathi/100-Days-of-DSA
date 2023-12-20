dd5499f64ca1d095669066bff27245096abb42d3
Shubhi Tripathi <shubhi.tripathi2020@vitbhopal.ac.in>
2023-12-20 10:15:33 +0530

    Fibonacci Number

---
+++ FibonacciNumber.cpp
@@ -0,0 +1,40 @@
+/* LINK : https://leetcode.com/problems/fibonacci-number/
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+*/
+#include<iostream>
+using namespace std;
+class Solution {
+ public:
+  int fib(int n) {
+    if (n < 2)
+      return n;
+
+    vector<int> dp{0, 0, 1};
+
+    for (int i = 2; i <= n; ++i) {
+      dp[0] = dp[1];
+      dp[1] = dp[2];
+      dp[2] = dp[0] + dp[1];
+    }
+
+    return dp.back();
+  }
+};
